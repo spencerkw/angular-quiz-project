c8f1bdc9764d68839dd0877d30d6a1075dd3aeca
Spencer K-W <keashlywarters@gmail.com>
2019-06-06 16:40:57 -0400

    prepared front-end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QuizComponent } from './quiz/quiz.component';
+import { ResultsComponent } from './results/results.component';
+import { ScoresComponent } from './scores/scores.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: "", redirectTo: "/quiz" },
+  { path: "quiz", component: QuizComponent },
+  { path: "results", component: ResultsComponent },
+  { path: "scores", component: ScoresComponent },
+  { path: "**", redirectTo: "/quiz" }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { ResultsComponent } from './results/results.component';
+import { ScoresComponent } from './scores/scores.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizComponent,
+    ResultsComponent,
+    ScoresComponent
   ],
   imports: [
     BrowserModule,
