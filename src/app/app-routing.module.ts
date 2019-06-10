45fd8092e9b2530d61f4a2f6332a575dd63dd211
Spencer K-W <keashlywarters@gmail.com>
2019-06-10 13:23:33 -0400

    pages now scroll to previous position when changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(appRoutes, {scrollPositionRestoration: "enabled"})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
